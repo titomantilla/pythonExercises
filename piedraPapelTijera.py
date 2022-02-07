100ee2cef32ceab4df5c67837f5227b48b84cc05
Tito Mantilla <mantilla.ernesto@gmail.com>
2022-02-07 19:02:28 -0300

    Some changes made

--- piedraPapelTijera.py
+++ piedraPapelTijera.py
@@ -5,7 +5,7 @@ userName = 0
 userWins = 0
 cpuWins = 0
 userPick = 0
-cpuPick = 0
+cpuPick = ["Rock", "Paper", "Scissors"] # List so the random library can choose the CPU options at random
 
 print(welcomeMessage.center(20,"+"))
 print("What is your name?")
@@ -13,4 +13,7 @@ userName = input()
 print("Welcome %s to the rock paper scissors game")
 print("Choose - Rock - Paper - Scissors")
 try:
-    
+    print("%sRock, paper or Scissors?",userName)
+    userPick = input()
+
+
